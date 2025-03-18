00501bd13092ccc5ce92af81a91003d9e0cbf987
michalDror <michal65918@gmail.com>
2025-03-18 21:30:32 +0200

    finaly

--- TodoApi/ToDoDbContext.cs
+++ TodoApi/ToDoDbContext.cs
@@ -20,21 +20,21 @@ public partial class ToDoDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("ConnectionStrings_ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
-// protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-// {
-//     if (!optionsBuilder.IsConfigured)
-//     {
-//         var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings_ToDoDB");
-//         if (string.IsNullOrEmpty(connectionString))
-//         {
-//             throw new Exception("Connection string 'ToDoDB' is not configured.");
-//         }
-//         optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
-//     }
-// }
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//         => optionsBuilder.UseMySql("ConnectionStrings_ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+{
+    if (!optionsBuilder.IsConfigured)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings_ToDoDB");
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new Exception("Connection string 'ToDoDB' is not configured.");
+        }
+        optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql"));
+    }
+}
 
 
 
